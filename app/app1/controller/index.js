de64c962d1d8e6e8a141990e6a14a4dc94db3fa2
mdemo <mengdesen09@qq.com>
2015-03-31 18:04:17 +0800

    add apps

---
+++ app/app1/controller/index.js
@@ -0,0 +1,6 @@
+/**
+ * bd-app-structure - index.js
+ * @mdemo
+ */
+
+'use strict';
--- index.js
+++ index.js
@@ -7,7 +7,19 @@
 
 var koa = require('koa');
 
+var router = router();
+
 var app = koa();
+
+//auto-load middleware
+
+var app1 =  router('app1');
+var app2 =  router('app2');
+
+// auto-load app1 middleware
+// auto-load app1 routes
+// auto-load app1 static
+// auto-load app1 other
 
 var server = app.listen(3000, function(){
   console.log(server.address().port)
